e6202924f8eca80b95fcfb022a62d94913b5b61f
Illia Bohush <bim22082004@gmail.com>
2023-12-26 14:48:30 +0000

    lab3

---
+++ test/kpi_backend/module/schemas.py
@@ -0,0 +1,37 @@
+from marshmallow import Schema, fields, validates_schema, ValidationError
+from .models import UserModel, CategoryModel
+
+
+class UserSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.Str(required=True)
+
+    @validates_schema
+    def validate_name(self, data, **kwargs):
+        name = data.get("name")
+
+        if UserModel.query.filter_by(name=name).count():
+            raise ValidationError(f"Name {name} already exists.")
+
+
+class RecordSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(required=True)
+    category_id = fields.Integer(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    sum = fields.Float(required=True)
+
+
+class CategorySchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+    user_id = fields.Integer()
+
+    @validates_schema
+    def validate_unique_category_name(self, data, **kwargs):
+        name = data.get('name')
+        user_id = data.get('user_id')
+
+        existing_category = CategoryModel.query.filter_by(name=name, user_id=user_id).first()
+        if existing_category:
+            raise ValidationError(f'Category with name {name} already exists for this user.')
