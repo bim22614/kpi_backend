91326086b79bd8c5cf3042d81c32bbab7262e50c
Illia Bohush <bim22082004@gmail.com>
2023-12-26 15:01:58 +0000

    bug fix

--- module/test.py
+++
@@ -1,3 +0,0 @@
-import os
-
-print(f'postgresql://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@db:{os.environ.get("DB_PORT")}/{os.environ.get("DB_NAME")}')
--- test/kpi_backend/.flaskenv
+++
@@ -1,1 +0,0 @@
-FLASK_APP=module/app.py
