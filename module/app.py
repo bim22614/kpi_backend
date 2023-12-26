b91e496255dd7ad89c0d6e56514c50dfb37e4637
bim22614 <64481896+bim22614@users.noreply.github.com>
2023-12-26 18:59:22 +0200

    Update app.py

--- module/app.py
+++ module/app.py
@@ -6,36 +6,48 @@ from .models import UserModel, CategoryModel, RecordModel
 from .init_data import users, categories, records
 from .views import views_blueprint
 from datetime import datetime
+import time
 
 
 app = Flask(__name__)
 app.app_context().push()
 
+
+time.sleep(30)
+print("befor app.config.from_pyfile")
+
 app.config.from_pyfile('config.py', silent=True)
 app.register_blueprint(views_blueprint)
 
+
+time.sleep(30)
+print("init")
+
 db.init_app(app)
 migrate = Migrate(app, db)
+
+time.sleep(30)
+print("create")
 
 db.create_all()
 
 def initialize_users():
     tables = [UserModel, CategoryModel, RecordModel]
-    
+
     if all(not table.query.first() for table in tables):
         for user_data in users:
             user = UserModel(**user_data)
             db.session.add(user)
-        
+
         for category_data in categories.values():
             category = CategoryModel(**category_data)
             db.session.add(category)
-        
+
         for record_data in records.values():
             record_data["created_at"] = datetime.strptime(record_data["created_at"], "%Y-%m-%dT%H:%M:%S")
             record = RecordModel(**record_data)
             db.session.add(record)
-        
+
         db.session.commit()
         print("Data added to the database.")
     else:
